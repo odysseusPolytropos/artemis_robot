54fe62a11aa0c2c3c4dae352428f29bd9575158f
Immanuel Löwenstein <immanuel.loewenstein@campus.tu-berlin.de>
2017-04-03 00:31:14 +0200

    [install_dependencies] added update and cd ..

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo apt-get update
 sudo apt-get install build-essential ros-jade-gazebo-ros     ros-jade-eigen-conversions     ros-jade-roslint libgazebo5 libsdformat2 libgazebo5-dev ros-jade-ros-control ros-jade-ros-controllers ros-jade-moveit-ros-visualization ros-jade-moveit ros-jade-gazebo-ros-control ros-jade-control-toolbox ros-jade-object-recognition-ros ros-jade-shape-tools ros-jade-xacro ros-jade-shape-msgs ros-jade-robot-state-publisher ros-jade-joint-state-publisher ros-jade-object-recognition-msgs ros-jade-control-msgs ros-jade-nav-msgs ros-jade-moveit-kinematics
 
 sudo ln -s /usr/include/eigen3/Eigen /usr/local/include/Eigen.
@@ -19,4 +19,4 @@ git clone https://github.com/odysseusPolytropos/gazebo-pkgs.git
 git clone https://github.com/odysseusPolytropos/general-message-pkgs.git
 git clone https://github.com/odysseusPolytropos/convenience-pkgs.git
 
-
+cd ..
